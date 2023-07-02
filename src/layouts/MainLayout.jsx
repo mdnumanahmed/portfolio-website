53c97b2120939966b00c5b1a914ab21d13292057
mdnumanahmed <mnumansyl@gmail.com>
2023-07-02 09:43:59 +0600

    Footer added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import MyNavBar from "../shared/MyNavBar/MyNavBar";
+import MyFooter from "../shared/MyFooter/MyFooter";
 
 const MainLayout = () => {
   return (
     <>
       <MyNavBar />
       <Outlet />
+      <MyFooter />
     </>
   );
 };
